7e626c94f8d9a848827e3becbbe2e9706312104b
lekhanhlinh-star <lekhanhlinh20002@gmail.com>
2025-10-08 19:41:51 +0700

    fix model ollama

--- run_fastapi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-Script để chạy ứng dụng FastAPI Healthcare Chatbot
-"""
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=5012, reload=True, debug=True)
